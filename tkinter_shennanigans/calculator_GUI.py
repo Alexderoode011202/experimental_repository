4ed6b1b380a384c6b21b67e341d432fd42fac4ff
Alex de Roode <Alexderoode011202@gmail.com>
2023-02-05 01:46:20 +0100

    finished my calculator

--- tkinter_shennanigans/calculator_GUI.py
+++ tkinter_shennanigans/calculator_GUI.py
@@ -2,7 +2,7 @@ from calculator_class import Calculator
 from calculator_class import itersplit
 from tkinter import *
 from functools import partial
-from typing import List, Dict
+from typing import List, Dict, Union
 
 root = Tk()
 calc = Calculator()
@@ -11,27 +11,54 @@ operators: List[str] = ['+', '-', '*', "/"]
 operators_button_list: List[Button] = []
 number_button_list: List[Button] = []
 
+# Create and grid the screen
+screen = Entry(root, width=10)
+screen.grid(column=0, row=0)
 
 # Create the numbers
-for num in range(1,10):
-    number_button_list.append(Button(root, text= str(num), width=10, height=3, command= partial(calc.add_number, str(num))))
-zero = Button(root, text= "0", height=3, width=10, command=partial(calc.add_number, '0'))
-filler_1 = Label(root, height=3, width=10)
-filler_2 = Label(root, height=3, width=10)
+number_button_list.append(Button(root, text = "1", width=10, height=3, command= lambda: [calc.add_number("1"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "2", width=10, height=3, command= lambda: [calc.add_number("2"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "3", width=10, height=3, command= lambda: [calc.add_number("3"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "4", width=10, height=3, command= lambda: [calc.add_number("4"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "5", width=10, height=3, command= lambda: [calc.add_number("5"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "6", width=10, height=3, command= lambda: [calc.add_number("6"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "7", width=10, height=3, command= lambda: [calc.add_number("7"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "8", width=10, height=3, command= lambda: [calc.add_number("8"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+number_button_list.append(Button(root, text = "9", width=10, height=3, command= lambda: [calc.add_number("9"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())]))
+
+zero = Button(root, text= "0", height=3, width=10, command=lambda: [calc.add_number("0"), screen.delete(0, "end"), screen.insert(0,calc.get_current_num())])
+filler_1 = Button(root, height=3, width=10, text = "-del", command= lambda: [calc.remove_number(), screen.delete(0, "end"), screen.insert(0, calc.get_current_num())])
+
+def calculate_results() -> None:
+    answer: Union[float, int] = calc.calculate()
+    screen.delete(0, "end")
+    screen.insert(0, f"{answer}")
+
+filler_2 = Button(root, height=3, width=10, text="=", command=calculate_results)
+    
 # some_list = [filler_1, zero, filler_2]
 number_button_list.extend([filler_1, zero, filler_2])
 
-# Grid the numbers
+# Grid the numbers + diverse
+print(f"list length: {len(number_button_list)}")
 split_num_but_list: List[List[Widget]] = itersplit(iterable=number_button_list, n_chunks= 4)
 for row_num, row in enumerate(split_num_but_list):
     for column, button in enumerate(row):
-        print(f"row: {row_num}, column: {column}")
-        button.grid(row=row_num, column=column)
+        # print(f"row: {row_num}, column: {column}")
+        button.grid(row=row_num+1, column=column)
 
+def oper_caller(operator: str):
+    calc.add_operator(operator= operator)
+    screen.delete(0, "end")
+    
 # Create the operators
-for op in operators:
-    operators_button_list.append(Button(root, text=op, width = 10, height=3, command= partial(calc.add_operator, op)))
+print("operator creation")
+for num, op in enumerate(operators):
+    print(op)
+    operators_button_list.append(Button(root, text=op, width = 10, height=3, command= partial(oper_caller, op)))
 
-
+# Grid the operators
+for num ,op in enumerate(operators_button_list):
+    op.grid(column=3, row =num+1)
 
 root.mainloop()
--- tkinter_shennanigans/calculator_class.py
+++ tkinter_shennanigans/calculator_class.py
@@ -15,6 +15,8 @@ class Calculator:
         self.operator_list: List[str] = []
         self.current_index : int = 0
         self.current_num: str = ""
+        self.result: Union[int, float] = 0
+
 
 
     def add_number(self, num: str) -> None:
@@ -32,7 +34,7 @@ class Calculator:
             return None
         else:
             self.current_num = self.current_num + num
-
+        print(f"current: {self.current_num}")
         return None
 
 
@@ -46,6 +48,7 @@ class Calculator:
             return None
         else:
             current_num = current_num[:-1]
+
         return None
 
     def add_operator(self, operator: str):
@@ -55,13 +58,12 @@ class Calculator:
         :returns: None
         """
 
-        if type(operator) != type("test"):
-            num = str(num)
-
         self.current_index += 1
         self.operator_list.append(operator)
+        print(operator)
         self.num_list.append(self.current_num)
         self.current_num = ""
+        print(f"current: {self.current_num}")
         return None
     
     def calculate(self) -> Union[int, float]:
@@ -69,31 +71,72 @@ class Calculator:
         calculates using the given numbers and operators to perform its calculations
         :returns: result in the form of an integer or float
         """
-        result: Union[float, int] = None
-        leftover_op: str 
-        self.operator_list.append(None)
+        # Add final number to the number list
+        self.num_list.extend([self.current_num, 'end'])
+        self.current_num = ""
+
+        self.operator_list.append('end')
+
+        # set up the entire calculation
         num_and_op: List[tuple[str, str]] = zip(self.num_list, self.operator_list)
+        full_calculation: List[Union[int, str]] = []
 
+        print(f"numbers: {self.num_list}")
+        print(f"operators: {self.operator_list}")
+        # create the full sum: e.g: 3+4+5
         for num, op in num_and_op:
-            # Very first part
-            if result == None:
-                result = int(num)
-                leftover_op = op
-
-            # Everything else
+            if num == "end" or op == None:
+                full_calculation.extend([num, op])
             else:
-                if leftover_op == "-":
-                    result = result- int(num)
-                elif leftover_op == "+":
-                    result = result + int(num)
-                elif leftover_op == "*":
-                    result = result * int(num)
-                elif leftover_op == "/":
-                    result = result/int(num)
-                leftover_op = op
+                full_calculation.extend([int(num), op])
+        
+        print(full_calculation)
+        for index in range(0,len(full_calculation) -1, 2):
 
-        return result
+            print(f"stuff: {full_calculation[index]}")
+            num_one: int = full_calculation[index]
+            try:
+                operator: str = full_calculation[index+1]
+            except IndexError:
+                print(f"result: {num_one}")
+                self.start_over()
+                return num_one
+            try:
+                num_two: int = full_calculation[index+2]
+            except IndexError:
+                print(f"result: {num_one}")
+                self.start_over()
+                return num_one
 
+            if not operator or not num_two:
+                print(f"result: {num_one}")
+                return num_one
+            
+            else:
+                if operator == "+":
+                    full_calculation[index+2] = num_one + num_two
+                elif operator == "*":
+                    full_calculation[index+2] = num_one * num_two
+                elif operator == "/":
+                    full_calculation[index+2] = num_one / num_two
+                elif operator == "-":
+                    full_calculation[index+2] = num_one-num_two
+        
+        return None
+
+    def get_current_num(self) -> str:
+        return self.current_num
+
+    def get_result(self) -> str:
+        return str(self.result)
+
+    def start_over(self) -> None:
+        "Sets all the arguments to their standard value"
+        self.num_list: List[str] = []
+        self.operator_list: List[str] = []
+        self.current_index : int = 0
+        self.current_num: str = ""
+        self.result: Union[int, float] = 0
 
 # Is meant to be used to the GUI
 def itersplit(iterable: Iterable, n_chunks: int) -> List[List[any]]:
@@ -101,13 +144,11 @@ def itersplit(iterable: Iterable, n_chunks: int) -> List[List[any]]:
         raise UnsplittableException
     seperated_list: list = []
     chunk_size: int = int(len(iterable)/n_chunks)
-    print(f"CHUNK_SIZE: {chunk_size}")
 
     for i in range(0, len(iterable), chunk_size):
         seperated_list.append(iterable[i:i+chunk_size])
 
     return seperated_list
-
 
 # for i in range(0, len(data), 100):
 #   chunk = data[i:i + 100]
