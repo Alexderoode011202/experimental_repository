4748ec4c70ac5e3ba15515d4b3d3498fef8af4e2
Alex de Roode <Alexderoode011202@gmail.com>
2023-02-13 17:30:29 +0100

    fixed almost all of pong

--- pygame_shenanigans/pong/direction_tracker.py
+++ pygame_shenanigans/pong/direction_tracker.py
@@ -5,7 +5,8 @@ from typing import Tuple
 
 class DirectionTracker():
     def __init__(self):
-        self.current = random.choice(range(0,361))
+        # self.current = random.choice(range(3, 360))
+        self.current = 45
 
     def get_direction(self) -> int:
         return self.current
@@ -19,10 +20,9 @@ class DirectionTracker():
         midle: int
 
         if self.current < 90:
-            midle = 360
-            ...
-            self.current = midle - self.current
-        elif 90 < self.current <180:
+            angle = self.current
+            self.current = 360 - angle
+        elif 90 <= self.current <180:
             midle = 180
             angle = midle - self.current
             angle = math.sqrt(angle**2)
@@ -34,11 +34,12 @@ class DirectionTracker():
             angle = math.sqrt(angle**2)
             self.current = midle - angle
 
-        elif self.current > 270:
-            midle = 270
+        elif self.current >= 270:
+            print("TEST")
+            midle = 360
             angle = midle - self.current
             angle = math.sqrt(angle**2)
-            self.current = midle - angle
+            self.current = angle
 
     def invert_vertical(self) -> None:
         """
@@ -60,48 +61,52 @@ class DirectionTracker():
             self.current = middle - angle
 
         elif 180 < self.current <=270:
+            print("BOTTOM TEST")
             middle = 270
             angle = middle-self.current
             angle = math.sqrt(angle**2)
             self.current = middle + angle
+
         elif self.current > 270:
+            print("high")
             middle = 270
-            angle = middle = self.current
+            angle = self.current - 270
             angle = math.sqrt(angle**2)
             self.current = middle-angle
 
     def get_grid_movement(self) -> Tuple[int, int]:
         x_axis: int
         y_axis: int
-        degrees_per_grid = 360/40
-        grid_point: int = floor(self.current/ degrees_per_grid)
-        # up -> y-axis +
-        # down -> y-axis -
+        grid_point: int = floor(self.current/ 9)
+        # up -> y-axis -
+        # down -> y-axis +
         # left -> x-axis -
         # right -> x-axis +
 
         # right
-        if 5 < grid_point <=15:
+        if 5 <= grid_point <=15:
             x_axis = 5
-            y_axis = 10 - grid_point
+            y_axis = grid_point - 10
 
-        # left
-        elif 25 < grid_point <= 35:
-            x_axis = -5
-            y_axis = grid_point - 30
+        # first 5
+        elif grid_point < 5:
+            y_axis = -5
+            x_axis = grid_point
+
+        # last 5
+        elif grid_point >= 35:
+            y_axis = -5
+            x_axis = grid_point - 40
 
         # bottom
-        elif 15 < grid_point <= 25:
-            y_axis = -5
-            x_axis = 20 - grid_point
-        
-        # top
-        elif grid_point >= 35 or grid_point <= 5:
+        elif 15 <= grid_point <= 25:
             y_axis = 5
-            if grid_point >= 35:
-                x_axis = grid_point - 40
-            else:
-                x_axis = grid_point
+            x_axis = 20 - grid_point 
+
+        # left side
+        elif 25 < grid_point <35:
+            x_axis = -5
+            y_axis = 30 - grid_point
 
         return (x_axis, y_axis)
 
--- pygame_shenanigans/pong/pong_attempt.py
+++ pygame_shenanigans/pong/pong_attempt.py
@@ -1,6 +1,9 @@
 import pygame
+from failure_screen import failure_screen
 from pygame import Surface, Rect
 from direction_tracker import DirectionTracker
+
+
 pygame.init()
 
 WIDTH: int = 800
@@ -24,7 +27,7 @@ p2_rect: Rect = p2_bat.get_rect(center=(WIDTH/16*15, LENGTH*0.5))
 ball: Surface = pygame.image.load("pygame_shenanigans/pong/ball.png")
 ball_rect = ball.get_rect(center=(WIDTH*0.5, LENGTH*0.5))
 dt= DirectionTracker()
-
+speed = 1
 
 
 while running:
@@ -40,24 +43,50 @@ while running:
             if event.key == pygame.K_s:
                 p1_rect.top +=30
     print(f"{ball_rect.top} and {ball_rect.bottom}")
+    print(dt.get_direction())
     # colision rules:
     if ball_rect.top < 0:
         print("TOP REACHED")
+        ball_rect.centery += 5
         dt.invert_vertical()
+    
+
     elif ball_rect.bottom > LENGTH:
+        # ball_rect.bottom - 5
         print("BOTTOM REACHED")
         dt.invert_vertical()
+        
 
-    if ball_rect.colliderect(p1_rect):
+    if ball_rect.right == WIDTH:
+        # ball_rect.right = -5
         dt.invert_horizontal()
 
+    if ball_rect.centerx == 0:
+        pygame.quit()
+        pygame.init()
+        failure_screen()
+        
+       
+
+    if ball_rect.colliderect(p1_rect):
+        ball_rect.left + 5
+        speed +=0.05
+        dt.invert_horizontal()
+
+    if p2_rect.colliderect(ball_rect):
+        speed+=0.05
+        dt.invert_horizontal()
+        
+
+
     #ball logic
-    ball_rect.centerx = ball_rect.centerx + dt.get_grid_movement()[0]
-    ball_rect.centery = ball_rect.centery + dt.get_grid_movement()[1]
+    ball_rect.centerx = ball_rect.centerx + dt.get_grid_movement()[0] * speed
+    ball_rect.centery = ball_rect.centery + dt.get_grid_movement()[1] * speed
 
 
     screen.fill((0,0,0))
     screen.blit(p1_bat, p1_rect)
+    p2_rect.centery = ball_rect.centery
     screen.blit(p2_bat, p2_rect)
     screen.blit(ball, ball_rect)
     pygame.display.update()
